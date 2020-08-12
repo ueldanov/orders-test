e93651606745853cf7ad2fa16f817f8d6e01e6da
Karim <ikrmuk@gmail.com>
2020-08-12 16:37:00 +0300

    Order migrations

---
+++ migrations/m200812_062528_create_order_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%order}}`.
+ */
+class m200812_062528_create_order_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%order}}', [
+            'id' => $this->primaryKey(),
+            'status' => $this->integer()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%order}}');
+    }
+}
